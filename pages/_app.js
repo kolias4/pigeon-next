958d37f31b9dd69de8cabc6d21e0d852ee0f9acc
gknikos3 <gknikos3@gmail.com>
2020-12-28 00:50:01 +0200

    css order change

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 
 
 
-import '../styles/alogosite/js_composer.min.css'
+
 import '../styles/alogosite/bootstrap-grid.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/alogosite/plugins.css'
 import '../styles/alogosite/style.css'
+
 import '../styles/alogosite/inline.css'
+import '../styles/alogosite/js_composer.min.css'
 import '../styles/alogosite/font-awesome.min.css'
 
 
