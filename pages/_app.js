721ae072417823b092f3737ae801b248e7960163
gknikos3 <gknikos3@gmail.com>
2020-12-28 00:19:31 +0200

    add font awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,20 @@
-import '../styles/globals.css'
+
+
+
+import '../styles/alogosite/js_composer.min.css'
+import '../styles/alogosite/bootstrap-grid.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/alogosite/plugins.css'
+import '../styles/alogosite/style.css'
+import '../styles/alogosite/inline.css'
+import '../styles/alogosite/font-awesome.min.css'
+
+
+
+
+
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
